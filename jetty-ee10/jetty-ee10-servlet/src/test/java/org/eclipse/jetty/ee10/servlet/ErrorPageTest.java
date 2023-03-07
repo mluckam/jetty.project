5e47e725f686682094ef89039d89056bc136e8c6
gregw <gregw@webtide.com>
2023-03-07 22:39:32 +0100

    BadMessage interface
    Convert class BadMessageException to BadMessage interface that allows different types of BadMessage exceptions.
    This follows the pattern of the QuietException interface.

--- jetty-ee10/jetty-ee10-servlet/src/test/java/org/eclipse/jetty/ee10/servlet/ErrorPageTest.java
+++ jetty-ee10/jetty-ee10-servlet/src/test/java/org/eclipse/jetty/ee10/servlet/ErrorPageTest.java
@@ -123,7 +123,7 @@ public class ErrorPageTest
         _errorPageErrorHandler.addErrorPage(400, "/error/400");
         // error.addErrorPage(500,"/error/500");
         _errorPageErrorHandler.addErrorPage(IllegalStateException.class.getCanonicalName(), "/error/TestException");
-        _errorPageErrorHandler.addErrorPage(BadMessage.RuntimeException.class, "/error/BadMessageException");
+        _errorPageErrorHandler.addErrorPage(BadMessage.RuntimeException.class, "/error/BadMessage");
         _errorPageErrorHandler.addErrorPage(ErrorPageErrorHandler.GLOBAL_ERROR_PAGE, "/error/GlobalErrorPage");
         _errorPageErrorHandler.addErrorPage(TestServletException.class, "/error");
 
@@ -390,11 +390,11 @@ public class ErrorPageTest
         {
             String response = _connector.getResponse("GET /app?baa=%88%A4 HTTP/1.0\r\n\r\n");
             assertThat(response, Matchers.containsString("HTTP/1.1 400 Bad Request"));
-            assertThat(response, Matchers.containsString("ERROR_PAGE: /BadMessageException"));
+            assertThat(response, Matchers.containsString("ERROR_PAGE: /BadMessage"));
             assertThat(response, Matchers.containsString("ERROR_MESSAGE: Unable to parse URI query"));
             assertThat(response, Matchers.containsString("ERROR_CODE: 400"));
-            assertThat(response, Matchers.containsString("ERROR_EXCEPTION: org.eclipse.jetty.http.BadMessageException: 400: Unable to parse URI query"));
-            assertThat(response, Matchers.containsString("ERROR_EXCEPTION_TYPE: class org.eclipse.jetty.http.BadMessageException"));
+            assertThat(response, Matchers.containsString("ERROR_EXCEPTION: org.eclipse.jetty.http.BadMessage$RuntimeException: 400: Unable to parse URI query"));
+            assertThat(response, Matchers.containsString("ERROR_EXCEPTION_TYPE: class org.eclipse.jetty.http.BadMessage$RuntimeException"));
             assertThat(response, Matchers.containsString("ERROR_SERVLET: org.eclipse.jetty.ee10.servlet.ErrorPageTest$AppServlet-"));
             assertThat(response, Matchers.containsString("ERROR_REQUEST_URI: /app"));
             assertThat(response, Matchers.containsString("getParameterMap()= {}"));
